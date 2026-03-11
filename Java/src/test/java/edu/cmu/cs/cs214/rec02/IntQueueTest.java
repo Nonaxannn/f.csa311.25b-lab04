e77d3b798cba4f22f2d265560306f57f8df8f8fb
Nonaxannn <nomin9302@gmail.com>
2026-03-11 13:51:15 +0800

    lab4

--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -1,16 +1,18 @@
 package edu.cmu.cs.cs214.rec02;
  
-import org.junit.Before;
-import org.junit.Test;
- 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
- 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
  
  
 /**
